1ba860ed42ed79c2325a77423d68a0efe16eb942
ss5429ss18 <102012128+ss5429ss18@users.noreply.github.com>
2022-04-01 14:28:36 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -6,9 +6,9 @@ public class Hello {
 		System.out.println("login");
 		System.out.println("logout");
 		system.out.println("Register");
-		system.out.println("what is this?")
+		system.out.println("what is Git?")
 		system.out.println("That's DVCS!");
-		System.out.println("hello");
-			system.out.println("Hello Git3");
+		System.out.println("Hello Git2");
+		system.out.println("Hello Git3");
 	}
 }
